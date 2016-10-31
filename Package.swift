56d7a23e8b6a3362611e31cb496b64f0e2420b02
Jonathan Guthrie <jono@perfect.org>
2016-10-30 22:37:30 -0400

    convert to swift 3, and Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-09-17 01:35:49 +0000
+import PackageDescription
+let package = Package(
+    name: "LoremSwiftum",
+    targets: [],
+    dependencies: [
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git",majorVersion: 0, minor: 2),
+		.Package(url: "https://github.com/iamjono/SwiftString.git",majorVersion: 1, minor: 0)
+	],
+    exclude: []
+)
