fa2e42ee20cd822721b75e8a93a98be14fb1900e
S. Ryan Quick <ryan@phaedo.com>
2019-06-19 18:17:57 +0200

    syntax

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@
 import PackageDescription
 let package = Package(
     name: "LoremSwiftum",
-	products: [
-		.library(name: "LoremSwiftum", targets: ["LoremSwiftum"])
-	],
+	products: [ .library(name: "LoremSwiftum", targets: ["LoremSwiftum"]) ],
     dependencies: [
 		.package(url: "https://github.com/providentiaww/SwiftRandom.git", from: "5.0.0"),
 		.package(url: "https://github.com/providentiaww/SwiftString.git", from: "5.0.0")
